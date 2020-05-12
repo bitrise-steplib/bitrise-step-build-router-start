dc94d85fcd133f072133fd7fc750cf315523c2db
Laszlo Pusok <laszlo.pusok@bitrise.io>
2020-05-12 09:41:09 +0200

    Adding retryable HTTP request

--- bitrise/bitrise.go
+++ bitrise/bitrise.go
@@ -6,7 +6,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
+
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 // Build ...
@@ -50,22 +54,83 @@ type Environment struct {
 
 // App ...
 type App struct {
+	BaseURL           string
 	Slug, AccessToken string
+	IsDebug           bool
+}
+
+func NewAppWithDefaultURL(slug, accessToken string) App {
+	return App{
+		BaseURL:     "https://api.bitrise.io",
+		Slug:        slug,
+		AccessToken: accessToken,
+	}
+}
+
+type RetryLogAdaptor struct{}
+
+func (*RetryLogAdaptor) Printf(fmtStr string, vars ...interface{}) {
+	switch {
+	case strings.HasPrefix(fmtStr, "[DEBUG]"):
+		log.Printf(strings.TrimSpace(fmtStr[7:]), vars...)
+	case strings.HasPrefix(fmtStr, "[ERR]"):
+		log.Errorf(strings.TrimSpace(fmtStr[5:]), vars...)
+	case strings.HasPrefix(fmtStr, "[ERROR]"):
+		log.Errorf(strings.TrimSpace(fmtStr[7:]), vars...)
+	case strings.HasPrefix(fmtStr, "[WARN]"):
+		log.Warnf(strings.TrimSpace(fmtStr[6:]), vars...)
+	case strings.HasPrefix(fmtStr, "[INFO]"):
+		log.Infof(strings.TrimSpace(fmtStr[6:]), vars...)
+	default:
+		log.Printf(fmtStr, vars...)
+	}
+}
+
+func NewRetryableClient(isDebug bool) *retryablehttp.Client {
+	client := retryablehttp.NewClient()
+	client.CheckRetry = retryablehttp.DefaultRetryPolicy
+	client.Backoff = retryablehttp.DefaultBackoff
+	client.Logger = &RetryLogAdaptor{}
+	client.ErrorHandler = retryablehttp.PassthroughErrorHandler
+	if !isDebug {
+		client.RetryWaitMin = 10 * time.Second
+		client.RetryWaitMax = 60 * time.Second
+		client.RetryMax = 5
+	} else {
+		client.RetryWaitMin = 100 * time.Millisecond
+		client.RetryWaitMax = 400 * time.Millisecond
+		client.RetryMax = 3
+	}
+
+	return client
 }
 
 // GetBuild ...
-func (app App) GetBuild(buildSlug string) (Build, error) {
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://api.bitrise.io/v0.1/apps/%s/builds/%s", app.Slug, buildSlug), nil)
+func (app App) GetBuild(buildSlug string) (build Build, err error) {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/v0.1/apps/%s/builds/%s", app.BaseURL, app.Slug, buildSlug), nil)
 	if err != nil {
 		return Build{}, err
 	}
 
 	req.Header.Add("Authorization", "token "+app.AccessToken)
 
-	resp, err := http.DefaultClient.Do(req)
+	retryReq, err := retryablehttp.FromRequest(req)
+	if err != nil {
+		return Build{}, fmt.Errorf("failed to create retryable request: %s", err)
+	}
+
+	client := NewRetryableClient(app.IsDebug)
+
+	resp, err := client.Do(retryReq)
 	if err != nil {
 		return Build{}, err
 	}
+
+	defer func() {
+		if cerr := resp.Body.Close(); cerr != nil && err == nil {
+			err = cerr
+		}
+	}()
 
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -76,15 +141,15 @@ func (app App) GetBuild(buildSlug string) (Build, error) {
 		return Build{}, fmt.Errorf("failed to get response, statuscode: %d, body: %s", resp.StatusCode, respBody)
 	}
 
-	var build buildResponse
-	if err := json.Unmarshal(respBody, &build); err != nil {
+	var buildResponse buildResponse
+	if err := json.Unmarshal(respBody, &buildResponse); err != nil {
 		return Build{}, fmt.Errorf("failed to decode response, body: %s, error: %s", respBody, err)
 	}
-	return build.Data, nil
+	return buildResponse.Data, nil
 }
 
 // StartBuild ...
-func (app App) StartBuild(workflow string, buildParams json.RawMessage, buildNumber string, environments []Environment) (StartResponse, error) {
+func (app App) StartBuild(workflow string, buildParams json.RawMessage, buildNumber string, environments []Environment) (startResponse StartResponse, err error) {
 	var params map[string]interface{}
 	if err := json.Unmarshal(buildParams, &params); err != nil {
 		return StartResponse{}, err
@@ -111,16 +176,29 @@ func (app App) StartBuild(workflow string, buildParams json.RawMessage, buildNum
 		return StartResponse{}, nil
 	}
 
-	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("https://api.bitrise.io/v0.1/apps/%s/builds", app.Slug), bytes.NewReader(b))
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/v0.1/apps/%s/builds", app.BaseURL, app.Slug), bytes.NewReader(b))
 	if err != nil {
 		return StartResponse{}, nil
 	}
 	req.Header.Add("Authorization", "token "+app.AccessToken)
 
-	resp, err := http.DefaultClient.Do(req)
+	retryReq, err := retryablehttp.FromRequest(req)
+	if err != nil {
+		return StartResponse{}, fmt.Errorf("failed to create retryable request: %s", err)
+	}
+
+	retryClient := NewRetryableClient(app.IsDebug)
+
+	resp, err := retryClient.Do(retryReq)
 	if err != nil {
 		return StartResponse{}, nil
 	}
+
+	defer func() {
+		if cerr := resp.Body.Close(); cerr != nil && err == nil {
+			err = cerr
+		}
+	}()
 
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
